b5533e600bff7712fa1307add6f33e6f86835fb0
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-10-21 05:06:31 +0900

    Initialize with random unitary

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ type FourByFour<T> = Four<Four<T>>;
 
 // Mat[i][j] is ith row, jth column
 type Mat = FourByFour<Complex>;
+type Vec = Four<Complex>;
 
 const zero: () => Mat = () => {
     return [
@@ -26,14 +27,54 @@ const random_gaussian: () => Mat = () => {
     return ans;
 }
 
-const test = random_gaussian();
- 
+const scale: (scaling: Complex, u: Vec) => Vec = (scaling: Complex, u: Vec) => [
+    u[0].mul(scaling),
+    u[1].mul(scaling),
+    u[2].mul(scaling),
+    u[3].mul(scaling),
+];
+
+const dot: (u: Vec, v: Vec) => Complex = (u: Vec, v: Vec) =>
+    Array.from({ length: 4 }, (_, i) => u[i].conjugate().mul(v[i])).reduce((a, b) => a.add(b), Complex.ZERO);
+
+const proj: (u: Vec, v: Vec) => Vec = (u: Vec, v: Vec) => scale(dot(u, v).div(dot(u, u)), u);
+
+const normalize: (u: Vec) => Vec = (u: Vec) => scale(dot(u,u).sqrt().inverse(), u);
+
+const sub: (u: Vec, v: Vec) => Vec = (u: Vec, v:Vec) => [
+    u[0].sub(v[0]),
+    u[1].sub(v[1]),
+    u[2].sub(v[2]),
+    u[3].sub(v[3]),
+]
+
+const gram_schmidt: (vs: Mat) => Mat = (vs: Mat) => {
+    const u0 = vs[0]; 
+    const e0 = normalize(u0);
+    const u1 = sub(vs[1], proj(u0, vs[1])); 
+    const e1 = normalize(u1);
+    const u2 = sub(sub(vs[2], proj(u0, vs[2])), proj(u1, vs[2]));
+    const e2 = normalize(u2);
+    const u3 = sub(sub(sub(vs[3], proj(u0, vs[3])), proj(u1, vs[3])), proj(u2, vs[3]));
+    const e3 = normalize(u3);
+    return [e0, e1, e2, e3];
+}
+
+const random_unitary: () => Mat = () => {
+    // According to https://mathoverflow.net/questions/333187/random-unitary-matrices :
+    // > A simple method starts from an N×N matrix filled with independent Gaussian random variables [Complex for U(N) and real for O(N)]. 
+    // > Then orthonormalize the columns via Gram-Schmidt and you're done.
+    return gram_schmidt(random_gaussian());
+}
+
+const test = random_unitary();
+
 const render = (a: Mat) => {
     const svg = document.getElementById("matrix")!;
     const SPACING = 100;
     const X_OFFSET = 50;
     const Y_OFFSET = 50;
-    const CIRCLE_MAX_RADIUS = 25;
+    const CIRCLE_MAX_RADIUS = 40;
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
             const center = {
